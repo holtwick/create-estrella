799cb1cb04d8ae472cf67acaf439f6c7d52ad60f
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-10-01 09:36:27 +0200

    Reduce to the minimal setup as discussed here https://github.com/rsms/estrella/issues/9#issuecomment-701124622

--- template/build.js
+++ template/build.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
-const pkg = require('./package.json')
-const common = require('./build.config.js')
-const { build } = require('estrella')
+const { build } = require("estrella")
+const pkg = require("./package.json")
 
 build({
-  ...common,
-  entry: 'src/index.js',
-  outfile: pkg.main
+  entry: "src/index.js",
+  outfile: pkg.main,
 })
