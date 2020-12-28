31aa3c6b622f1ff19d2146c7f419a35edd56ad14
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-12-28 11:35:01 +0100

    TS modules

--- template-ts-module/build.js
+++ template-ts-module/build.js
@@ -4,20 +4,17 @@ const { build } = require("estrella")
 const pkg = require("./package.json")
 
 build({
+  bundle: true,
+  sourcemap: true,
   format: "cjs",
   entry: "src/index.ts",
   outfile: pkg.main,
 })
 
 build({
+  bundle: true,
+  sourcemap: true,
   format: "esm",
   entry: "src/index.ts",
   outfile: pkg.module,
-})
-
-build({
-  format: "iife",
-  entry: "src/index.ts",
-  outfile: pkg.unpkg,
-  globalName: pkg.name,
 })
